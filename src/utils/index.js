a699cbb386e209dd7cb2ed0d527f3b6a306a5361
高力 <3071730@qq.com>
2018-09-04 19:43:46 +0800

    feat(utils): Add toQueryString into utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,10 +6,13 @@ import upperFirst from 'lodash/upperFirst';
 const createCounter = (count = 0) => () => count++;
 const createId = createCounter();
 
+const toQueryString = obj => Object.keys(obj).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`).join('&');
+
 export {
   createId,
   mapKeys,
   merge,
   pick,
   upperFirst,
+  toQueryString,
 };
