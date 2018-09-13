caf3b5c35d288f5c581912a22918bf2734b308de
高力 <3071730@qq.com>
2018-09-13 11:04:03 +0800

    chore: Add home page
    fix #58

---
+++ demo/src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Card } from 'antd';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <Row type="flex" style={{ flex: 1, alignItems: 'center' }}>
+        <Col span={4} />
+        <Col span={16}>
+          <Card title="DEMO">
+            <p><Link to="/flow">流程图</Link></p>
+            <p><Link to="/mind">思维导图</Link></p>
+          </Card>
+        </Col>
+        <Col span={4} />
+      </Row>
+    );
+  }
+}
+
+export default HomePage;
