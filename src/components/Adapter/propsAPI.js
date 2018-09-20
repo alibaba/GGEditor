f456996f10aee0ea0ec545ec3f9127e6c92d8109
高力 <3071730@qq.com>
2018-09-20 10:23:51 +0800

    refactor(propsAPI): Pass the methods of the current page

--- src/components/Adapter/propsAPI.js
+++ src/components/Adapter/propsAPI.js
@@ -1,23 +1,17 @@
-let editor;
-
 class PropsAPI {
-  constructor(_editor) {
-    editor = _editor;
+  editor = null;
+
+  constructor(editor) {
+    this.editor = editor;
+
+    ['read', 'save', 'add', 'find', 'update', 'remove', 'getSelected'].forEach((key) => {
+      this[key] = (...params) => this.currentPage[key](...params);
+    });
   }
 
   get currentPage() {
-    return editor.getCurrentPage();
+    return this.editor.getCurrentPage();
   }
-
-  read = data => this.currentPage.read(data);
-  save = () => this.currentPage.save();
-
-  add = (type, model) => this.currentPage.add(type, model);
-  find = id => this.currentPage.find(id);
-  update = (item, model) => this.currentPage.update(item, model);
-  remove = item => this.currentPage.remove(item);
-
-  getSelected = () => this.currentPage.getSelected();
 }
 
 export default PropsAPI;
