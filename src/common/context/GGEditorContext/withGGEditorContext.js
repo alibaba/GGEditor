39a3f17f5e9028e30edd202cc5fed0deabd2becc
高力 <3071730@qq.com>
2018-10-21 17:22:14 +0800

    feat(Context): Forward the `ref` property

--- src/common/context/GGEditorContext/withGGEditorContext.js
+++ src/common/context/GGEditorContext/withGGEditorContext.js
@@ -2,13 +2,17 @@ import React from 'react';
 import GGEditorContext from '@common/context/GGEditorContext';
 
 export default function (WrappedComponent) {
-  return class extends React.Component {
+  class InjectGGEditorContext extends React.Component {
     render() {
+      const { forwardRef, ...rest } = this.props;
+
       return (
         <GGEditorContext.Consumer>
-          {context => <WrappedComponent {...context} {...this.props} />}
+          {context => <WrappedComponent ref={forwardRef} {...rest} {...context} />}
         </GGEditorContext.Consumer>
       );
     }
-  };
+  }
+
+  return React.forwardRef((props, ref) => <InjectGGEditorContext {...props} forwardRef={ref} />);
 }
