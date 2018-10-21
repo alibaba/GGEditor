c370409bc362bf668392d40db1fda4e682ad835f
高力 <3071730@qq.com>
2018-10-21 17:22:14 +0800

    feat(Context): Forward the `ref` property

--- src/common/context/PropsAPIContext/withPropsAPI.js
+++ src/common/context/PropsAPIContext/withPropsAPI.js
@@ -2,13 +2,17 @@ import React from 'react';
 import PropsAPIContext from '@common/context/PropsAPIContext';
 
 export default function (WrappedComponent) {
-  return class extends React.Component {
+  class InjectPropsAPI extends React.Component {
     render() {
+      const { forwardRef, ...rest } = this.props;
+
       return (
         <PropsAPIContext.Consumer>
-          {propsAPI => <WrappedComponent propsAPI={propsAPI} {...this.props} />}
+          {propsAPI => <WrappedComponent ref={forwardRef} {...rest} propsAPI={propsAPI} />}
         </PropsAPIContext.Consumer>
       );
     }
-  };
+  }
+
+  return React.forwardRef((props, ref) => <InjectPropsAPI {...props} forwardRef={ref} />);
 }
