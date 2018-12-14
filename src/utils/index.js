2681afc28ea485ff93dee8c5fbdf183fc29421c9
高力 <3071730@qq.com>
2018-12-14 13:10:52 +0800

    feat(utils): Add the `toQueryString` method

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,9 +3,12 @@ import pick from 'lodash/pick';
 import uniqueId from 'lodash/uniqueId';
 import upperFirst from 'lodash/upperFirst';
 
+const toQueryString = obj => Object.keys(obj).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`).join('&');
+
 export {
   merge,
   pick,
+  toQueryString,
   uniqueId,
   upperFirst,
 };
