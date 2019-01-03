4bba85e78f20e9ace1fc6368ede81faa6b35ca68
高力 <3071730@qq.com>
2019-01-03 14:16:15 +0800

    feat(Minimap): Import the `Minimap` component directly from `G6`

--- src/components/Minimap/index.js
+++ src/components/Minimap/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import Editor from '@components/Base/Editor';
+import G6 from '@antv/g6';
 import { pick } from '@utils';
 import { MINIMAP_CONTAINER } from '@common/constants';
 import withGGEditorContext from '@common/context/GGEditorContext/withGGEditorContext';
 
+require('@antv/g6/build/plugin.tool.minimap');
+
+const { Minimap: G6Minimap } = G6.Components;
+
 class Minimap extends React.Component {
+  minimap = null;
+
   get containerId() {
     const { editor } = this.props;
 
     return `${MINIMAP_CONTAINER}_${editor.id}`;
   }
 
-  componentDidMount() {
+  get currentPage() {
     const { editor } = this.props;
+
+    return editor.getCurrentPage();
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.bindEvent();
+  }
+
+  componentDidMount() {
+    this.init();
+    this.bindPage();
+  }
+
+  init() {
     const {
       container = this.containerId,
       width,
@@ -23,13 +45,34 @@ class Minimap extends React.Component {
 
     const { clientWidth, clientHeight } = document.getElementById(container);
 
-    editor.add(new Editor.Minimap({
+    this.minimap = new G6Minimap({
       container,
       width: width || clientWidth,
       height: height || clientHeight,
       viewportWindowStyle,
       viewportBackStyle,
-    }));
+    });
+
+    this.minimap.getGraph = () => this.currentPage.getGraph();
+  }
+
+  bindPage() {
+    if (!this.minimap || !this.currentPage) {
+      return;
+    }
+
+    const graph = this.currentPage.getGraph();
+
+    this.minimap.bindGraph(graph);
+    this.minimap.debounceRender();
+  }
+
+  bindEvent() {
+    const { onAfterAddPage } = this.props;
+
+    onAfterAddPage(() => {
+      this.bindPage();
+    });
   }
 
   render() {
