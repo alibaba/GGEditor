c4fabd5fdbccc908a37af20a79104438cc8890b9
liuchengzhe <chengzhe.lcz@alibaba-inc.com>
2019-04-29 13:35:59 +0800

    refactor: optimize mind-node config object
    add more functions like getTextStyle, enable to config node style by different depth, etc.

--- src/components/Mind/shape/nodes/mind-node.js
+++ src/components/Mind/shape/nodes/mind-node.js
@@ -1,54 +1,99 @@
 import G6 from '@antv/g6';
+import Util from '../../util';
 
 G6.registerNode('mind-node', {
-  shapeType: 'rect',
   draw(cfg, group) {
-    console.log(cfg)
-    const keyShape = this.drawShape(cfg, group);
+    this.drawShape(cfg, group);
     if (cfg.data.label) {
       this.drawLabel(cfg, group);
     }
-    return keyShape;
+    this.adjustKeyShape();
+    this.adjustTextShape();
+    return this.keyShape;
   },
   drawShape(cfg, group) {
-    const {shapeType} = this;
     const shapeCfg = this.getShapeStyle();
-    // root node
-    if (cfg.isRoot()) {
-      return group.addShape(shapeType, {
-        attrs: Object.assign({
-          fill: '#587EF7',
-          stroke: '#587EF7',
-        }, shapeCfg),
-      });
-    }
-    return group.addShape(shapeType, {
-      attrs: Object.assign({
-        fill: '#fff',
-        stroke: '#dadada',
-      }, shapeCfg),
+    this.keyShape = group.addShape(this.getNodeShape(), {
+      attrs: {
+        x: 0,
+        y: 0,
+        ...Object.assign({}, shapeCfg.default, shapeCfg[cfg.depth]),
+      },
     });
+    return this.keyShape;
+  },
+  drawLabel(cfg, group) {
+    // 展示文本
+    const text = Util.getLabelTextByTextLineWidth(cfg.data.label, this.getMaxTextLineWidth());
+    // 绘制文本
+    this.textShape = group.addShape('text', {
+      attrs: {
+        text,
+        ...this.getTextStyle(),
+      },
+    });
+  },
+  adjustKeyShape() {
+    const padding = this.getPadding();
+    const originWidth = this.keyShape.attr('width');
+    const originHeight = this.keyShape.attr('height');
+    const [textWidth, textHeight] = [this.getTextSize().width, this.getTextSize().height];
+    if (originHeight < textHeight) {
+      this.keyShape.attr('height', textHeight + 2 * padding[0]);
+    }
+    if (originWidth < textWidth) {
+      this.keyShape.attr('width', textWidth + 2 * padding[1]);
+    }
+  },
+  adjustTextShape() {
+    this.textShape.attr('x', this.keyShape.attr('width') / 2);
+    this.textShape.attr('y', this.keyShape.attr('height') / 2);
   },
   getShapeStyle() {
     return {
-      x: 0,
-      y: 0,
-      width: 80,
-      height: 40,
+      default: {
+        width: 80,
+        height: 40,
+        fill: '#fff',
+        stroke: '#000',
+        radius: 5,
+      },
+      0: {
+        fill: '#419ee0',
+        stroke: '#4156e0',
+        radius: 20,
+      },
+      1: {
+        fill: '#eaeaea',
+        stroke: '#ccc',
+      },
     };
   },
-  drawLabel(cfg, group) {
-    // 绘制文本
-    return group.addShape('text', {
-      attrs: {
-        text: cfg.data.label,
-        x: 40,
-        y: 20,
-        fill: '#000',
-        textAlign: 'center',
-        textBaseline: 'middle',
-      },
-    });
+  getTextStyle() {
+    return {
+      fill: '#000',
+      fontSize: 12,
+      fontWeight: 'normal',
+      x: 40,
+      y: 20,
+      textAlign: 'center',
+      textBaseline: 'middle',
+    };
+  },
+  getNodeShape() {
+    return 'rect';
+  },
+  getMaxTextLineWidth() {
+    return 80;
+  },
+  getTextSize() {
+    return {
+      width: this.textShape.getBBox().width,
+      height: this.textShape.getBBox().height,
+    };
+  },
+  getPadding() {
+    return [5, 5];
   },
   getAnchorPoints() {
     return [
