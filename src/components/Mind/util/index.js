532e48592844bd309c6956c935d7349958898b43
liuchengzhe <chengzhe.lcz@alibaba-inc.com>
2019-04-29 15:21:09 +0800

    refactor: optimize Mind/util
    replace original function with optimizeMultilineText

--- src/components/Mind/util/index.js
+++ src/components/Mind/util/index.js
@@ -1,21 +1,30 @@
 const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d');
+const canvasContext = canvas.getContext('2d');
 
 const Util = {
-  getLabelTextByTextLineWidth(labelText, maxTextLineWidth = 320) {
-    let lineWidth = ctx.measureText(labelText).width;
-    if (lineWidth > maxTextLineWidth) {
-      lineWidth = 0;
-      for (let i = 1; i < labelText.length; i++) {
-        lineWidth += ctx.measureText(labelText[i]).width;
-        if (lineWidth >= maxTextLineWidth) {
-          labelText = `${labelText.substring(0, i)}\n${labelText.substring(i, labelText.length)}`;
-          i += 1;
-          lineWidth = 0;
-        }
-      }
+  optimizeMultilineText(text, font, maxWidth = 320) {
+    canvasContext.font = font;
+
+    if (canvasContext.measureText(text) <= maxWidth) {
+      return text;
     }
-    return labelText;
+
+    let multilineText = '';
+    let multilineTextWidth = 0;
+
+    for (const char of text) {
+      const { width } = canvasContext.measureText(char);
+
+      if (multilineTextWidth + width >= maxWidth) {
+        multilineText += '\n';
+        multilineTextWidth = 0;
+      }
+
+      multilineText += char;
+      multilineTextWidth += width;
+    }
+
+    return multilineText;
   },
 };
 
