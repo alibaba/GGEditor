2dd5e2f8862d4ee8233674767e76efcc1aa32968
liuchengzhe <chengzhe.lcz@alibaba-inc.com>
2019-04-29 11:02:43 +0800

    refactor: adjust label style

--- src/components/Mind/shape/nodes/mind-node.js
+++ src/components/Mind/shape/nodes/mind-node.js
@@ -3,31 +3,50 @@ import G6 from '@antv/g6';
 G6.registerNode('mind-node', {
   shapeType: 'rect',
   draw(cfg, group) {
+    console.log(cfg)
+    const keyShape = this.drawShape(cfg, group);
     if (cfg.data.label) {
       this.drawLabel(cfg, group);
     }
-    return this.drawShape(cfg, group);
+    return keyShape;
   },
   drawShape(cfg, group) {
-    const { shapeType } = this;
+    const {shapeType} = this;
+    const shapeCfg = this.getShapeStyle();
+    // root node
+    if (cfg.isRoot()) {
+      return group.addShape(shapeType, {
+        attrs: Object.assign({
+          fill: '#587EF7',
+          stroke: '#587EF7',
+        }, shapeCfg),
+      });
+    }
     return group.addShape(shapeType, {
-      attrs: {
-        x: 0,
-        y: 0,
-        stroke: '#ccc',
-        width: 80,
-        height: 40,
+      attrs: Object.assign({
         fill: '#fff',
-      },
+        stroke: '#dadada',
+      }, shapeCfg),
     });
+  },
+  getShapeStyle() {
+    return {
+      x: 0,
+      y: 0,
+      width: 80,
+      height: 40,
+    };
   },
   drawLabel(cfg, group) {
     // 绘制文本
     return group.addShape('text', {
       attrs: {
         text: cfg.data.label,
-        x: 0,
-        y: 0,
+        x: 40,
+        y: 20,
+        fill: '#000',
+        textAlign: 'center',
+        textBaseline: 'middle',
       },
     });
   },
