2fac4a5d9a2aa592888e3ad792ea0e06144639cc
高力 <3071730@qq.com>
2019-11-15 11:47:07 +0800

    chore: optimize the examples

---
+++ examples/plugin-item-popover/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GGEditor, { Flow } from '@/index';
+import ItemPopover from '@/plugins/ItemPopover';
+import data from '../mock/flow.json';
+import styles from './index.less';
+
+class Index extends React.Component {
+  renderTitle(/* item: G6.Item */) {
+    return 'Title';
+  }
+
+  renderContent(/* item: G6.Item */) {
+    return 'Content';
+  }
+
+  render() {
+    return (
+      <GGEditor className={styles.editor}>
+        <Flow
+          className={styles.editorBd}
+          data={data}
+          graphConfig={{
+            defaultNode: {
+              shape: 'bizNode',
+            },
+            defaultEdge: {
+              shape: 'bizFlowEdge',
+            },
+          }}
+        />
+        <ItemPopover renderTitle={this.renderTitle} renderContent={this.renderContent} />
+      </GGEditor>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
