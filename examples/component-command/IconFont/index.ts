9e9bf5c6a230bd62a5bb72890ec361d2547f46d1
高力 <3071730@qq.com>
2019-11-19 20:07:29 +0800

    improvement: optimize the `Command` component

---
+++ examples/component-command/IconFont/index.ts
@@ -0,0 +1,7 @@
+import { Icon } from 'antd';
+
+const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: 'https://at.alicdn.com/t/font_1518433_dpc9q9h1ti4.js',
+});
+
+export default IconFont;
--- src/components/Command/index.tsx
+++ src/components/Command/index.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
+import { EditorEvent } from '@/common/constants';
+import { GraphStateEvent } from '@/common/interfaces';
 import commandManager from '@/common/commandManager';
 import { EditorContextProps, withEditorContext } from '@/components/EditorContext';
 
 interface CommandProps extends EditorContextProps {
   name: string;
+  className?: string;
+  disabledClassName?: string;
 }
 
 interface CommandState {}
 
 class Command extends React.Component<CommandProps, CommandState> {
+  static defaultProps = {
+    className: 'command',
+    disabledClassName: 'command-disabled',
+  };
+
   state = {
     disabled: false,
   };
+
+  componentDidUpdate(prevProps: CommandProps) {
+    const { graph, name } = this.props;
+
+    if (graph === prevProps.graph) {
+      return;
+    }
+
+    this.setState({
+      disabled: !commandManager.canExecute(graph, name),
+    });
+
+    graph.on<GraphStateEvent>(EditorEvent.onGraphStateChange, () => {
+      this.setState({
+        disabled: !commandManager.canExecute(graph, name),
+      });
+    });
+  }
 
   handleClick = () => {
     const { name, executeCommand } = this.props;
@@ -20,17 +47,17 @@ class Command extends React.Component<CommandProps, CommandState> {
   };
 
   render() {
-    const { graph, name, children } = this.props;
+    const { graph } = this.props;
 
     if (!graph) {
       return null;
     }
 
+    const { className, disabledClassName, children } = this.props;
+    const { disabled } = this.state;
+
     return (
-      <div
-        className={`command${commandManager.canExecute(graph, name) ? '' : ' command-disabled'}`}
-        onClick={this.handleClick}
-      >
+      <div className={`${className}${disabled ? ` ${disabledClassName}` : ''}`} onClick={this.handleClick}>
         {children}
       </div>
     );
