208cdfadb0b5254c158e227bb1e8962f219a0f8d
高力 <3071730@qq.com>
2019-11-22 15:53:21 +0800

    refactor: refactor the `ItemPanel` component

---
+++ examples/component-item-panel/index.tsx
@@ -0,0 +1,151 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GGEditor, { Flow, Item, ItemPanel } from '@/index';
+import data from '../mock/flow.json';
+import styles from './index.less';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <GGEditor className={styles.editor}>
+        <ItemPanel className={styles.editorHd}>
+          <Item
+            className={styles.item}
+            model={{
+              shape: 'circle',
+              size: 100,
+              label: 'circle',
+            }}
+          >
+            <img
+              src="https://gw.alicdn.com/tfs/TB1IRuSnRr0gK0jSZFnXXbRRXXa-110-112.png"
+              width="55"
+              height="56"
+              draggable={false}
+            />
+          </Item>
+          <Item
+            className={styles.item}
+            model={{
+              shape: 'rect',
+              size: [100, 50],
+              label: 'rect',
+            }}
+          >
+            <img
+              src="https://gw.alicdn.com/tfs/TB1reKOnUT1gK0jSZFrXXcNCXXa-178-76.png"
+              width="89"
+              height="38"
+              draggable={false}
+            />
+          </Item>
+          <Item
+            className={styles.item}
+            model={{
+              shape: 'ellipse',
+              size: [100, 50],
+              label: 'ellipse',
+            }}
+          >
+            <img
+              src="https://gw.alicdn.com/tfs/TB1AvmVnUH1gK0jSZSyXXXtlpXa-216-126.png"
+              width="108"
+              height="63"
+              draggable={false}
+            />
+          </Item>
+          <Item
+            className={styles.item}
+            model={{
+              shape: 'diamond',
+              size: 100,
+              label: 'diamond',
+            }}
+          >
+            <img
+              src="https://gw.alicdn.com/tfs/TB1EB9VnNz1gK0jSZSgXXavwpXa-178-184.png"
+              width="89"
+              height="92"
+              draggable={false}
+            />
+          </Item>
+          <Item
+            className={styles.item}
+            model={{
+              shape: 'triangle',
+              size: 100,
+              label: 'triangle',
+            }}
+          >
+            <img
+              src="https://gw.alicdn.com/tfs/TB12sC2nKH2gK0jSZJnXXaT1FXa-126-156.png"
+              width="63"
+              height="78"
+              draggable={false}
+            />
+          </Item>
+          <Item
+            className={styles.item}
+            model={{
+              shape: 'star',
+              size: 100,
+              label: 'star',
+            }}
+          >
+            <img
+              src="https://gw.alicdn.com/tfs/TB1m4O1nO_1gK0jSZFqXXcpaXXa-194-198.png"
+              width="97"
+              height="99"
+              draggable={false}
+            />
+          </Item>
+          <Item
+            className={styles.item}
+            model={{
+              shape: 'image',
+              size: 100,
+              label: 'image',
+            }}
+          >
+            <img
+              src="https://gw.alicdn.com/tfs/TB1eP94nKH2gK0jSZJnXXaT1FXa-112-134.png"
+              width="56"
+              height="67"
+              draggable={false}
+            />
+          </Item>
+          <Item
+            className={styles.item}
+            model={{
+              shape: 'bizNode',
+              size: [112, 66],
+              label: 'bizNode',
+              center: 'topLeft',
+            }}
+          >
+            <img
+              src="https://gw.alicdn.com/tfs/TB1_cbfnHj1gK0jSZFuXXcrHpXa-244-132.png"
+              width="112"
+              height="66"
+              draggable={false}
+            />
+          </Item>
+        </ItemPanel>
+        <Flow
+          className={styles.editorBd}
+          data={data}
+          graphConfig={{
+            defaultNode: {
+              shape: 'bizNode',
+            },
+            defaultEdge: {
+              shape: 'bizFlowEdge',
+            },
+          }}
+        />
+      </GGEditor>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
--- src/components/ItemPanel/Item.tsx
+++ src/components/ItemPanel/Item.tsx
@@ -1,211 +1,38 @@
 import React from 'react';
 import pick from 'lodash/pick';
-import { ItemType } from '@/common/constants';
+import { ADD_NODE_MODEL, ItemType, GraphMode } from '@/common/constants';
+import { NodeModel, EdgeModel } from '@/common/interfaces';
 import { EditorContextProps, withEditorContext } from '@/components/EditorContext';
 
-interface ItemProps extends EditorContextProps {
-  /** 预览图资源 */
-  src: string;
-
-  /** 类型（边线或节点） */
-  type: 'node' | 'edge';
-
-  /** 形状 */
-  shape: string;
-
-  /** 节点model */
-  model: object;
-
-  /** 尺寸 */
-  size: string;
+export interface ItemProps extends EditorContextProps {
+  style?: React.CSSProperties;
+  className?: string;
+  type?: ItemType;
+  model: Partial<NodeModel> | Partial<EdgeModel>;
 }
 
-interface ItemState {
-  /** 隐藏的用于拖拽的DOM节点 */
-  shadowShape: null | HTMLElement;
+export interface ItemState {}
 
-  /** 画布上代理图形是否存在 */
-  hasDragShape: boolean;
-
-  /** 画布上代理图形的 ID（常量）*/
-  dragShapeID: 'temp_drag_node';
-}
-
-class Item extends React.PureComponent<ItemProps, ItemState> {
-  constructor(props: ItemProps) {
-    super(props);
-    this.state = {
-      shadowShape: null,
-      hasDragShape: false,
-      dragShapeID: 'temp_drag_node',
-    };
-  }
-
-  handleMouseDown = (ev: any) => {
-    const shadowShape = this.createShadowShape(ev);
-    document.body.appendChild(shadowShape);
-    this.setState({
-      shadowShape,
-    });
+class Item extends React.Component<ItemProps, ItemState> {
+  static defaultProps = {
+    type: ItemType.Node,
   };
 
-  handleMouseUp = () => {
-    this.unloadDragShape();
-  };
+  handleMouseDown = () => {
+    const { graph, type, model } = this.props;
 
-  createShadowShape(ev: any) {
-    const { src } = this.props;
-
-    const Img = document.createElement('img');
-    Img.src = src;
-    const shadowShape = document.createElement('div');
-    const styleObj = `
-      width: ${Img.width}px;
-      height: ${Img.height}px;
-      position: fixed;
-      opacity: 0;
-      top: ${ev.clientY - Img.height / 2}px;
-      left: ${ev.clientX - Img.width / 2}px;
-      cursor: pointer;
-      z-index:99999;
-    `;
-
-    shadowShape.setAttribute('style', styleObj);
-    shadowShape.setAttribute('draggable', 'true');
-    shadowShape.addEventListener('drag', this.handleDrag, false);
-    document.addEventListener('dragover', this.handleDragover, false);
-    document.addEventListener('dragenter', this.handleDragenter, false);
-    document.addEventListener('drop', this.handleDrop, false);
-    shadowShape.addEventListener('mouseup', this.handleMouseUp, false);
-    return shadowShape;
-  }
-
-  handleDragover = (ev: DragEvent) => {
-    ev.preventDefault();
-  };
-
-  handleDragenter = (ev: DragEvent) => {
-    const { graph } = this.props;
-
-    if (!graph) {
-      return;
+    if (type === ItemType.Node) {
+      graph.set(ADD_NODE_MODEL, model);
+      graph.setMode(GraphMode.AddNode);
     }
-
-    const transferredPos = graph.getPointByClient(ev.clientX, ev.clientY);
-
-    const canvas = graph.get('container').getElementsByTagName('canvas')[0];
-    // drag into canvas
-    if (ev.target && ev.target === canvas.id) {
-      this.loadDragShape(transferredPos);
-    }
-  };
-
-  handleDrag = (ev: DragEvent) => {
-    const { graph } = this.props;
-
-    if (!graph) {
-      return;
-    }
-
-    const { hasDragShape, dragShapeID } = this.state;
-    if (hasDragShape) {
-      const transferredPos = graph.getPointByClient(ev.clientX, ev.clientY);
-      graph.update(dragShapeID, {
-        ...transferredPos,
-      });
-    }
-  };
-
-  loadDragShape({ x, y }: { x: number; y: number }) {
-    const { graph } = this.props;
-    const { hasDragShape, shadowShape, dragShapeID } = this.state;
-
-    if (!graph || !shadowShape) {
-      return;
-    }
-
-    if (!hasDragShape) {
-      graph.add(ItemType.Node, {
-        shape: 'rect',
-        x,
-        y,
-        size: [shadowShape.offsetWidth, shadowShape.offsetHeight],
-        style: {
-          fill: '#F3F9FF',
-          fillOpacity: 0.5,
-          stroke: '#1890FF',
-          strokeOpacity: 0.9,
-          lineDash: [5, 5],
-        },
-        id: dragShapeID,
-      });
-
-      this.setState({
-        hasDragShape: true,
-      });
-    }
-  }
-
-  unloadDragShape() {
-    const { graph } = this.props;
-    const { hasDragShape, shadowShape, dragShapeID } = this.state;
-
-    if (!graph) {
-      return;
-    }
-
-    if (hasDragShape) {
-      graph.remove(dragShapeID);
-    }
-    if (shadowShape) {
-      document.body.removeChild(shadowShape);
-    }
-    this.setState({
-      hasDragShape: false,
-      shadowShape: null,
-    });
-    document.removeEventListener('dragenter', this.handleDragenter);
-    document.removeEventListener('dragover', this.handleDragover);
-    document.removeEventListener('drop', this.handleDrop);
-  }
-
-  handleDrop = (ev: any) => {
-    const { graph, executeCommand, type, model, shape, size } = this.props;
-    const { dragShapeID } = this.state;
-
-    if (!graph) {
-      return;
-    }
-
-    const canvas = graph.get('container').getElementsByTagName('canvas')[0];
-    const transferredPos = graph.getPointByClient(ev.clientX, ev.clientY);
-
-    // drag into canvas
-    if (ev.target && ev.target.id === canvas.id) {
-      executeCommand('add', {
-        type,
-        model: {
-          ...model,
-          shape,
-          ...transferredPos,
-          size: size.split('*'),
-        },
-      });
-    }
-    this.unloadDragShape();
-    graph.remove(dragShapeID);
   };
 
   render() {
-    const { src, shape, children } = this.props;
+    const { children } = this.props;
 
     return (
-      <div
-        style={{ cursor: 'pointer' }}
-        onMouseDown={this.handleMouseDown}
-        {...pick(this.props, ['style', 'className'])}
-      >
-        {src ? <img src={src} alt={shape} draggable={false} /> : children}
+      <div {...pick(this.props, ['style', 'className'])} onMouseDown={this.handleMouseDown}>
+        {children}
       </div>
     );
   }
