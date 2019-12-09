ce2c0e6fda56f08351470dd439f01506a81f3d8b
Liu CZ <425518173@qq.com>
2019-12-09 16:03:56 +0800

    improvement: add state hook to bizNode to enhance its extensible ability

--- src/components/Flow/shape/nodes/bizFlowNode.ts
+++ src/components/Flow/shape/nodes/bizFlowNode.ts
@@ -1,3 +1,16 @@
+import { ItemState } from '@/common/constants';
 import G6 from '@antv/g6';
+import { handleAnchor } from './anchor';
 
-G6.registerNode('bizFlowNode', {}, 'bizNode');
+G6.registerNode(
+  'bizFlowNode',
+  {
+    handleAnchor,
+
+    beforeSetState(name: ItemState, value: boolean, item: G6.Node) {
+      // 根据状态绘制锚点
+      this.handleAnchor.call(this, name, value, item);
+    },
+  },
+  'bizNode',
+);
